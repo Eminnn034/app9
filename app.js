4ac6581fad729e70045753346cb2b955dd1ff120
Eminnn034 <emin.seyid97@gmail.com>
2024-12-09 19:11:47 +0400

    tapsiriq9

---
+++ app.js
@@ -0,0 +1,42 @@
+let d = "32,35,37,39,41"
+let h = d.replaceAll("," ,";");
+console.log(h);
+
+
+
+
+
+let  names = ['Hofstadter','Cooper','Wolowtiz','Koothrappali'];
+arr.sort((a,b) => b-a);
+console.log(arr);
+
+
+
+
+
+
+let arr1 = [10,20,30,40,50];
+let main = [...arr]
+   let e = main.map((t)=>{
+
+    return t*2
+
+   })
+   console.log(e);
+
+
+
+   let arr2  =[10,20,30,40,50 ,60,70,80,90,100];
+
+let [c,b, ... orthers] = arr
+
+console.log(a);
+console.log(b);
+console.log(orthers);
+
+
+let   arr = [10,15,20,30,40,50,60,70,80,90,10]; 
+ let a = Math.max(...arr);
+ console.log(a);
+
+ 
